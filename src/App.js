cd3b60c5185cca1678bcd9768ded77a0394cc144
Giovanni Massiot <giovannimassiot.gm@gmail.com>
2023-09-18 15:10:26 +0200

    add react icons and google fonts

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 function App() {
   return (
     <div>
+      <div className="font-signature text-5xl">Giovanni Massiot</div>
       <h1 className="text-red-800">Hello, world!</h1>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    fontFamily: {
+      signature: ["Qwitcher Grypen"],
+    },
   },
   plugins: [],
 }
